1c84c7818983e63f2e9aae64d05ef43eeba5a72f
shivangii-sri <shivangisrivastava762@gmail.com>
2024-06-08 15:13:54 +0530

    Initial project setup.

---
+++ src/main/java/com/movieTimeTracker/movieTimeTracker/MovieTimeTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.movieTimeTracker.movieTimeTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieTimeTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieTimeTrackerApplication.class, args);
+	}
+
+}
