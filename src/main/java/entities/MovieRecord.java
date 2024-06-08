1bd723c561e01c510db2084f8d2c0d9a67268be5
shivangii-sri <shivangisrivastava762@gmail.com>
2024-06-08 22:51:45 +0530

    feat : added MovieRecord entity

---
+++ src/main/java/entities/MovieRecord.java
@@ -0,0 +1,27 @@
+package entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class MovieRecord {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String movieName;
+    private int timeSpentInMin;
+    private int rating;
+    private String comments;
+    private Date watchedDate;
+}
