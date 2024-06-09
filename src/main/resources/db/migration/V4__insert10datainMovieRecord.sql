c5ceb724a141f78f1626001c09764dbdd1e1a3f5
SUD77 <jaiswalsudhanshu20@gmail.com>
2024-06-09 20:19:18 +0530

    feat: inserted 10 rows in movie_record table

---
+++ src/main/resources/db/migration/V4__insert10datainMovieRecord.sql
@@ -0,0 +1,12 @@
+INSERT INTO `movie_record` (`comments`, `movie_name`, `rating`, `time_spent_in_min`, `content_type`, `from_date`, `to_date`)
+VALUES
+(NULL, 'The Purge 2013', 6, 40, 'movie', '2024-01-04 00:00:00.000000', NULL),
+(NULL, 'The Purge 2014', 6, 40, 'movie', '2024-01-04 00:00:00.000000', NULL),
+(NULL, 'The Purge 2016', 5, 40, 'movie', '2024-01-12 00:00:00.000000', NULL),
+(NULL, 'The Purge 2018', 1, 20, 'movie', '2024-01-12 00:00:00.000000', NULL),
+(NULL, 'The Purge 2021', 1, 30, 'movie', '2024-01-12 00:00:00.000000', NULL),
+(NULL, '12th Fail', 10, 140, 'movie', '2024-01-18 00:00:00.000000', NULL),
+(NULL, 'Saltburn 2023', 1, 50, 'movie', '2024-01-19 00:00:00.000000', NULL),
+(NULL, 'Role Play', 1, 30, 'movie', '2024-01-22 00:00:00.000000', NULL),
+(NULL, 'This means War', 6, 90, 'movie', '2024-01-22 00:00:00.000000', NULL),
+(NULL, 'The day after Tomorrow', 7, 100, 'movie', '2024-01-22 00:00:00.000000', NULL);
